2ca8dfc15c93440f37e25a3c4f6daf9c59d4fb9e
Tom Planche <Tomplanche@icloud.com>
2024-04-06 21:22:57 +0200

    [1] Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	// Imports
+	import { style_vars } from '$lib/globals';
+	import '$lib/styles/main.scss';
+
+	// Variables
+	let headerHeight = style_vars.header_height;
+	let padding = style_vars.main_padding;
+
+	export const padding_top = `calc(2 * ${padding} + ${headerHeight})`;
+</script>
+
+<main style="padding: 0 {padding};">
+	<slot />
+</main>
+
+<style lang="scss">
+	@import '$lib/styles/reset';
+	@import '$lib/styles/base';
+	@import '$lib/styles/variables';
+
+	main {
+		min-height: 100vh;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+
+		/* cursor: none; */
+
+		background-color: $app-bg-light;
+		transition: background-color 0.3s ease-in-out;
+
+		:global(body.dark) & {
+			color: $gh-text;
+			background-color: $app-bg-dark;
+		}
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  // Imports
+
+
+  // Variables
+
+
+  // Watchers
+
+
+  // Methods
+
+</script>
+
+<h1>PORTFOLIO-2024</h1>
+
+<style lang="scss">
+
+</style>
