b06efc2a204caa2082f0a84545041880e6f2e48c
Tom Planche <Tomplanche@icloud.com>
2025-01-08 16:58:07 +0100

    [1] (feat on HEAD) Initial commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  // Imports
+  import "$lib/styles/main.scss";
+
+  // Variables
+
+  // Functions
+</script>
+
+<main>
+
+  <slot></slot>
+</main>
+
+<style lang="scss">
+  @use '$lib/styles/variables';
+
+  main {
+    padding: variables.$main-padding;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    overflow: hidden;
+    position: relative;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Tom Planche</h1>
