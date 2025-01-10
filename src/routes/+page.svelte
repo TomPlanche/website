679cb35d0778e7a1efdfa9ed86360d3f73b2356d
Tom Planche <Tomplanche@icloud.com>
2025-01-10 19:26:26 +0100

    [12] (feat on work-in-progress) Better homepage.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,67 @@
 <script lang="ts">
-  import ScrambleText from '$lib/components/ScrambleText.svelte';
   import {gsap} from 'gsap';
+  import {onMount} from "svelte";
+
+  import ScrambleTextPlugin from "$lib/utils/ScrambleText";
+  import SplitText from "$lib/utils/SplitText";
+
+  gsap.registerPlugin(ScrambleTextPlugin);
+  gsap.registerPlugin(SplitText);
 
   // Bindings
+  let WIP: HTMLParagraphElement;
+
+  let title = "Tom Planche";
+
+  // Functions
+  onMount(() => {
+    const tl = gsap.timeline({
+      defaults: {
+        duration: 1,
+        ease: "power2.out",
+      },
+    });
+
+    const splittedTitle = new SplitText(WIP, {type: "chars"});
+
+    console.log(splittedTitle.chars);
+
+    tl
+      .set(splittedTitle.chars, {opacity: 0})
+      .to("h1",
+        {
+          scrambleText: {
+            text: title,
+            chars: title.replace(/a/g, '4').replace(/e/g, '3').replace(/o/g, 'O'),
+            revealDelay: 0.625
+          },
+          duration: 1
+        },
+      )
+      .fromTo(
+        splittedTitle.chars,
+        {
+          opacity: 0,
+          translateY: "1rem"
+        },
+        {
+          opacity: 1,
+          translateY: 0,
+          stagger: 0.01,
+          duration: 0.25
+        }
+      );
+
+  })
 </script>
 
 <section>
   <h1>
-    <ScrambleText
-        duration={1000}
-        scrambleSpeed={75}
-        text="Tom Planche"
-    />
+    Tom Planche
   </h1>
+  <p
+      bind:this={WIP}
+  >This website is a work in progress. Please check back later.</p>
 
 </section>
 
@@ -34,8 +83,16 @@
       text-align: left;
     }
 
-    h1 {
+    h1, p {
       text-shadow: 0 0 5px var(--text-color);
+    }
+
+    p {
+      font-family: "Monorama", sans-serif;
+      font-size: 3vw;
+      text-align: justify;
+      padding: 1rem;
+      line-height: 1.5;
     }
   }
 </style>
