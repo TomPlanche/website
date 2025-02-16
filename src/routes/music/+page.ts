bc34bd5e7eb89ddb01f2991c092ec769eec8f591
Tom Planche <Tomplanche@icloud.com>
2025-02-16 07:46:21 +0100

    [38] (fix on server) Used `$env/static/public` instead of `$env/dynamic/public`.

--- src/routes/music/+page.ts
+++ src/routes/music/+page.ts
@@ -5,14 +5,14 @@
  */
 
 import type {PageLoad} from "./$types";
-import {env} from '$env/dynamic/public';
+import {PUBLIC_API_ENDPOINT} from '$env/static/public';
 import axios from "axios";
 import {CurrentlyPlayedSong} from "$lib/types/song";
 import {songsStore} from "$lib/stores/songStore";
 
 export const load: PageLoad = async ({fetch}) => {
   try {
-    const songs = await axios.get(`${env.PUBLIC_API_ENDPOINT}/static/recent_play_counts.json`);
+    const songs = await axios.get(`${PUBLIC_API_ENDPOINT}/static/recent_play_counts.json`);
 
     if (songs.status === 200) {
       songsStore.set(songs.data.map((song) => CurrentlyPlayedSong.parse(song)));
