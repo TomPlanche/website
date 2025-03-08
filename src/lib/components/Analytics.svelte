9a34f088b207187990b02a52c764add3d52cb0ed
Tom Planche <Tomplanche@icloud.com>
2025-03-08 03:27:12 +0100

    [80] (test on stats) Test for IP.

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -4,10 +4,22 @@
   // Replace with your Stats server URL
   const STATS_URL = "http://127.0.0.1:5775";
 
-  onMount(() => {
+  onMount(async () => {
     const head = document.head || document.getElementsByTagName("head")[0];
+    let ip = '';
+
+    try {
+      const response = await fetch('/api/ip');
+      const data = await response.json();
+      ip = data.ip;
+    } catch (error) {
+      console.error('Erreur lors de la récupération de l\'IP:', error);
+    }
+
+    console.log('IP:', ip);
+
     const script = document.createElement("script");
-    script.setAttribute("src", "http://localhost:5775/stats.js"); // REPLACE WITH ACTUAL URL
+    script.setAttribute("src", `${STATS_URL}/stats.js`);
     script.setAttribute("onload", () => window.collectStats());
     script.setAttribute("type", "text/javascript");
     script.setAttribute("charset", "utf8");
